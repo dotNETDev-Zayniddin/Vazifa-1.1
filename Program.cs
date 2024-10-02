416d0d25f3cc7f367bc4025c96e99359becf12d8
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-03 01:15:06 +0500

    Vazifa-1.1: Project Upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.NetworkInformation;
+namespace Vazifa {
+    class Program{
+        static void Main(string[] args){
+            decimal Pi = 3.1415926535898m;
+            System.Console.Write("Please enter a radius: ");
+            decimal Radius = Convert.ToDecimal(Console.ReadLine());
+            decimal  S;
+            S = Pi*Radius*Radius;
+            decimal L;
+            L = 2*Pi*Radius;
+            System.Console.WriteLine($"S = {S}");
+            System.Console.WriteLine($"L = {L}");
+
+
+        }
+    }
+}
